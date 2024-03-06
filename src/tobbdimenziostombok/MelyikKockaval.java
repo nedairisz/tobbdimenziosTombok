95e3fc2ccee226ff76e901121205488efca28204
nedairisz <iriszneda@gmail.com>
2024-03-06 10:58:17 +0100

    .

--- src/tobbdimenziostombok/MelyikKockaval.java
+++ src/tobbdimenziostombok/MelyikKockaval.java
@@ -8,22 +8,22 @@ public class MelyikKockaval {
     static Random rnd = new Random();
     
     public static void main(String[] args) {
-        final int SOR_DB= 6; //dobas_db
-        final int OSZL_DB= 6; //KOcka_oldal
+        final int DOBAS_DB= 50; 
+        final int KOCKA_OLDAL= 6;
         
-        int[][] dobasok = new int [SOR_DB+1][OSZL_DB+1];
+        int[][] dobasok = new int [DOBAS_DB+1][KOCKA_OLDAL+1];
         
         /*feltöltés*/
-        for (int i = 0; i < SOR_DB; i++) {
-            int dobas1 = (int) (Math.random()*OSZL_DB)+1;
-            int dobas2 = (int) (Math.random()*OSZL_DB)+1;
+        for (int i = 0; i < DOBAS_DB; i++) {
+            int dobas1 = (int) (Math.random()*KOCKA_OLDAL)+1;
+            int dobas2 = (int) (Math.random()*KOCKA_OLDAL)+1;
             
             dobasok[dobas1][dobas2]++;
         }
         
         /*megjelenítés*/
-        for (int i = 0; i < OSZL_DB; i++) {
-            for (int j = 0; j < OSZL_DB; j++) {
+        for (int i = 0; i < KOCKA_OLDAL; i++) {
+            for (int j = 0; j < KOCKA_OLDAL; j++) {
                 System.out.printf("%3d", dobasok[i][j]);
             }
             System.out.println("");
